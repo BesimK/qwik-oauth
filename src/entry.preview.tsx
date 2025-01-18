ffec640d8854a94002b5df78475e51b6e3b724ed
BesimK <besimkarabiyikk@gmail.com>
2025-01-18 23:43:54 +0300

    rest

--- src/entry.preview.tsx
+++ src/entry.preview.tsx
@@ -18,4 +18,8 @@ import render from "./entry.ssr";
 /**
  * The default export is the QwikCity adapter used by Vite preview.
  */
-export default createQwikCity({ render, qwikCityPlan, checkOrigin: false });
+const { router, notFound, staticFile } = createQwikCity({
+  render,
+  qwikCityPlan,
+  checkOrigin: false,
+});
